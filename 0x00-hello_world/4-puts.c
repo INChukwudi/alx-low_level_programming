48979753e5c6709c19140f2e143238259d31670a
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-02 16:17:42 -0700

    added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (void)
+/**
+ * main - this function creates and string and prints it out
+ *
+ * Return: 0 ti indicate the end of the function
+ */
+int main(void)
 {
 	char str1[52];
+	
 	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 	puts(str1);
 	return(0);
