3995abb79ccda3889caf0c694eb9b10329bc329a
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-02 16:24:40 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program. it prints a string
+ *
+ * Return: 0 to end the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
