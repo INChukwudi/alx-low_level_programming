6c659c9f697b3b69c8fa8f5a31b0f5d8cce5a8d3
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-03 13:17:15 -0700

    added 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char num;
+	int num;
 
-	for (num = '1'; num <= '9'; num++)
-		putchar(num);
+	for (num = 1; num < 10; num++)
+		putchar((char) num);
 
 	putchar('\n');
 	return (0);
