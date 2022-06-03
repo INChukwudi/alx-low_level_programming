9cadda6e5128ed278dbbdfdece9f53bdec8a0336
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-03 14:14:38 -0700

    added 100*

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		putchar(num1 + '0');
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
+			putchar(num1 + '0');
 			putchar(num2 + '0');
 			if (num1 != 8 && num2 != 9)
 			{
