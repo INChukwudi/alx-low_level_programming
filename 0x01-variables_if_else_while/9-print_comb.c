fc72db34e4fb814041670b1cd798a7af930d975d
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-03 13:49:09 -0700

    added 9*

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints out the lowercase letters
+ *
+ * Return: 0 to indicate end of program
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
