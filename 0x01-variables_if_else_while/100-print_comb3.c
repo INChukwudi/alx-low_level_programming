044edfe72ec0c52cef2e59d6b01a1ca831a9c0dc
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-03 14:05:26 -0700

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints out the lowercase letters
+ *
+ * Return: 0 to indicate end of program
+ */
+int main(void)
+{
+	int num1;
+       	int num2 = 1;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		putchar(num1 + '0');
+		for (num2; num2 < 10; num2++)
+		{
+			putchar(num2 + '0');
+			if (num1 != 8 && num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
