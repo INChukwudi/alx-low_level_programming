9e3ae89aa7f4ad7af352a8f151871cf227fc9156
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-03 13:38:42 -0700

    added 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function to print numbers in base 16
+ *
+ * Return: 0 to indicate end of function
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0x0; num <= 0xF; num++)
+	{
+		char* base16 = (char*) &num;
+		putchar(base16);
+	}
+	putchar('\n');
+	return (0);
+}
