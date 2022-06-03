f4b94832b49b4c02130effd9f1c763696591d3eb
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-03 13:13:34 -0700

    added 6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints out the single digit numbers of base 10
+ *
+ * Return: 0 to indicate end of program
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '1'; num < '10'; num++)
+		putchar(num);
+
+	putchar('\n');
+	return (0);
+}
