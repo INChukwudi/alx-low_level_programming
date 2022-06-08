9d11c05ae702f6bb3704709da47ba8b495d4cc99
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-07 22:49:53 -0700

    edited 100*

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,13 +29,15 @@ void print_times_table(int n)
 				if ((d / 10) > 0)
 					_putchar (((d / 10) % 10) + '0');
 				else
+				{
 					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar((d % 10) + '0');
 
 				if (col < n)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
