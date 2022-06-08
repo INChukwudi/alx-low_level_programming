59caea3bbbdc55097b9f3295bf68b4748e867271
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-07 22:27:47 -0700

    added 100*

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - print out the times yable for a number n
+ * @n: number
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		_putchar('n')
+	}
+	else
+	{
+		int row, col, d;
+
+		for (row = 0; row <= n; row++)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			for (col = 1; col <= n; col++)
+			{
+				d = (row * col);
+				if ((d / 10) > 0)
+					_putchar((d / 10) + '0');
+				else
+					_putchar(' ');
+				_putchar((d % 10) + '0');
+
+				if (col < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+
+}
