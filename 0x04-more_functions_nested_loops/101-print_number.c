35c83e6f671d6455ca42bccbb911ebe44d5c80c6
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-09 17:21:31 -0700

    added 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,19 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	if (n != 0)
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+
+		if (n / 10)
+			print_number(n / 10);
+
+		_putchar((n % 10) + '0');
 	}
-
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
+	else
+		_putchar('0');
 }
