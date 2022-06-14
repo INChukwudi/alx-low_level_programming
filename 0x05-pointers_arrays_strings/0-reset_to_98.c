fac92da7fe1c7088674584fee2f377772b8141b7
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-13 20:11:37 -0700

    added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates its value
+ * @n: the pointer to the int
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Reademe for this directory
