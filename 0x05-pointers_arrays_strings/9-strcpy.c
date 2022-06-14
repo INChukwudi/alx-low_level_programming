c83cd427616101d488415438e89ca9318e49067f
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-13 21:20:48 -0700

    added 9*

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - cpoies a string pointed to by @src including the
+ * 	terminating null byte to a buffer pointed to by @dest.
+ * @dest: pointer to buffer
+ * @src: pointer to string
+ *
+ * Return: a pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
