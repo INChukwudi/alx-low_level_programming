9f387a87a035f7f20ae4bfdd4c191cf8276cf0b9
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-22 20:58:04 -0700

    added 6*

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int find_sqrt(int num, int root);
+int prime_finder(int num, int div);
+int is_prime_number(int n);
