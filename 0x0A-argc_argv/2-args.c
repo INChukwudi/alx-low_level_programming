a2d0e62b026914b7f367ce0bf258ce7f9e5b11c4
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-24 12:48:30 -0700

    added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <std.io.h>
+
+/**
+ * main - prints all arguments the function receives
+ * @argc: number of arguments received
+ * @argv: deferenced array of arguments
+ *
+ * Return: 0 to terminate program
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
