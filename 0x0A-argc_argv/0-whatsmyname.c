056737aa047847cc507528f4665a6137203e8926
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-24 12:28:31 -0700

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name followed by a new line
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 to terminate program
+ */
+int main(void argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Readme for the 0x0A C - argc, argv
