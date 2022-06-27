f39590a3dd22714e8b146e7bc933d16eee39a461
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-27 13:40:05 -0700

    added 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -39,7 +39,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			twoD[i][j] = 0;
+			two_dim_arr[i][j] = 0;
 	}
 
 	return (two_dim_arr);
