c6540c9f664438088777960449a6ea2d22e7876a
Chukwudi <ikenwakochukwudi@gmail.com>
2022-06-30 23:34:33 -0700

    added 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <atdlib.h>
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: the first element of the array
+ * @max: the last element of the array
+ *
+ * Return: pointer to the newly created array
+ *         NULL if @min > @max OR malloc fails
+ */
+int *array_range(int min, int max)
+{
+	int *array, index, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
+}
