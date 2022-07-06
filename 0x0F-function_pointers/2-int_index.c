b07eceecd3d7ba2e5bc72ea9c1420354e0fc87fa
Chukwudi <ikenwakochukwudi@gmail.com>
2022-07-06 14:38:56 -0700

    added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.c"
+
+/**
+ * int_index - searches for an integer
+ * @array: the haystack
+ * @size: the size of the array
+ * @cmp: pointer that compares the values
+ *
+ * Return: -1 if no element matches
+ *         -1 if size is <= 0
+ *         index of the firs occurence of the needle
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	while (size-- > 0)
+	{
+		if (cmp(*array) != 0)
+			return cmp(*array);
+
+		array++;
+	}
+
+	return (-1);
+}
