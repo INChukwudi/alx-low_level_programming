2450a1348c6db85e7a502579388ef8b61b060933
Chukwudi <ikenwakochukwudi@gmail.com>
2022-07-06 14:44:52 -0700

    refactored 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,18 +13,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int index;
+
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
-	if (size <= 0)
-		return (-1);
-
-	while (size-- > 0)
+	for (index = 0; index < size; index++)
 	{
-		if (cmp(*array) != 0)
-			return cmp(*array);
-
-		array++;
+		if(cmp(array[index]) != 0)
+			return (index);
 	}
 
 	return (-1);
