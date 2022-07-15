da825f20c965632c4125f9e83be3ce81b77fcebd
Chukwudi <ikenwakochukwudi@gmail.com>
2022-07-14 20:50:33 -0700

    added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) startup(void);
+
+/**
+ * startup - prints string before main function is executed
+ */
+void startup(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
