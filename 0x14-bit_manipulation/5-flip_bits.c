eb76950c6167fb5eb91d022950c5e0c175970794
Chukwudi <ikenwakochukwudi@gmail.com>
2022-07-21 13:20:04 -0700

    refactored 5-*

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,16 +10,12 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int xor = n ^ m;
-	int count = 0;
+	unsigned int xor = n ^ m, count = 0;
 
-	if (!n || !m)
-		return (0);
-	while (xor)
+	while (xor > 0)
 	{
-		if (xor & 1)
-			count++;
-		xor = xor >> 1;
+		count += (xor & 1);
+		xor >> 1;
 	}
 	return (count);
 }
