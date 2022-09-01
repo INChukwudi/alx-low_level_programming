6d6a3a7dd6c2e538e33fe91ddc5a3f7226d24243
Chukwudi <ikenwakochukwudi@gmail.com>
2022-09-01 15:18:01 -0700

    added 1*

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dlistint_s - doubly libked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ */
 typedef struct dlistint_s
 {
 	int n;
@@ -12,5 +18,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LISTS_H */
