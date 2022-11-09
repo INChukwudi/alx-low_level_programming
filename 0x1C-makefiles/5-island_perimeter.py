f65fcd695fd15dfe0479cab709f1cc506e42e52d
Chukwudi <ikenwakochukwudi@gmail.com>
2022-11-09 15:14:39 -0800

    added skeleton for 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines the island_perimeter function
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimater of the island described in grid
+
+    Args:
+        grid (list): list of list of integers
+    """
+
+    return 0
